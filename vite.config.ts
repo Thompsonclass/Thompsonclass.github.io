86042da3a467f64f45914d75e7345b133413f284
Thompsonclass <bbb7219@naver.com>
2025-01-26 16:33:04 +0900

    main

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: "/",
+  base: "/Thompsonclass.github.io/",
   plugins: [react()],
   resolve: {
     alias: [
       { find: '@src', replacement: path.resolve(__dirname, 'src') },
       { find: '@components', replacement: path.resolve(__dirname, 'src/components') }
     ]
-  }
+  },
 });
