92a4e511983fc5f30bae428c7b5317ca7aafc3ec
Thompsonclass <bbb7219@naver.com>
2025-01-26 17:05:08 +0900

    mian

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client'; // createRoot를 import 해야 합니다.
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         {/* 추가적인 라우트가 필요하다면 여기에 추가 */}
