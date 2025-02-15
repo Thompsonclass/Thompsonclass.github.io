b7504b34119ceced10a32e3339b6e984220a0362
Thompsonclass <bbb7219@naver.com>
2025-02-16 01:49:21 +0900

    update

--- src/components/MemoItem.tsx
+++ src/components/MemoItem.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface MemoItemProps {
     task: string;
     index: number;
-    onDelete: (index: number) => void;
+    onDelete: (index: number) => void; //onDelete는 index: number를 받고, 반환값이 없는(void) 함수
 }
 
 const MemoItem: React.FC<MemoItemProps> = ({ task, index, onDelete }) => {
